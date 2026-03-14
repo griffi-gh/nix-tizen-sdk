920c9092733c8556d7a744102b887a2d6bd139ce
griffi-gh <prasol258@gmail.com>
2026-03-14 21:59:31 +0100

    split distribution.nix

---
+++ distribution.nix
@@ -0,0 +1,63 @@
+{ ... }:
+{
+  perSystem =
+    { pkgs, ... }:
+    let
+      inherit (pkgs) lib;
+    in
+    {
+      legacyPackages.tizen-sdk.distribution =
+        let
+          index = lib.importJSON ./data/index.json;
+        in
+        lib.mapAttrs (
+          distribution_name: distribution:
+          lib.mapAttrs' (snapshot_name: snapshot: {
+            name = lib.replaceString "." "_" snapshot_name;
+            value = lib.mapAttrs (
+              os: object_path:
+              let
+                packages = lib.importJSON ./${object_path};
+              in
+              lib.listToAttrs (
+                map (
+                  package:
+                  let
+                    pname = lib.replaceString "." "_" package.package;
+                  in
+                  {
+                    name = pname;
+                    value =
+                      let
+                        inherit (pkgs) stdenvNoCC fetchurl unzip;
+                      in
+                      stdenvNoCC.mkDerivation {
+                        # TODO: respect dependencies
+                        inherit pname;
+                        inherit (package) version;
+                        src = fetchurl {
+                          url = "https://download.tizen.org/sdk/tizenstudio/${distribution_name}${package.path}";
+                          inherit (package) sha256;
+                        };
+                        nativeBuildInputs = [ unzip ];
+                        unpackPhase = ''
+                          unzip $src
+                        '';
+                        installPhase = ''
+                          mv data $out
+                        '';
+                        meta = {
+                          inherit (package) description; # (prob should be longDescription)
+                          license = lib.licenses.unfree;
+                          sourceProvenance = with lib.sourceTypes; [ binaryNativeCode ];
+                          homepage = "https://developer.tizen.org/development/tizen-studio/download/";
+                        };
+                      };
+                  }
+                ) packages
+              )
+            ) snapshot.packages;
+          }) distribution.snapshots
+        ) index;
+    };
+}
