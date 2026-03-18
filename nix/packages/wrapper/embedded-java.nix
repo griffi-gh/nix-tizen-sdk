88e8d2c91164c0c937b43c0857e94fdecc5eb1ae
griffi-gh <prasol258@gmail.com>
2026-03-18 12:12:12 +0100

    meow

--- nix/packages/wrapper/embedded-java.nix
+++ nix/packages/wrapper/embedded-java.nix
@@ -2,13 +2,15 @@
   runCommand,
   stdenvNoCC,
   javaPackages,
+  tizenSdkDistribution,
   ...
 }:
 let
+  inherit (tizenSdkDistribution.ubuntu-64) embedded-java; # (actually unused)
   jdk = javaPackages.compiler.openjdk8;
 in
 runCommand "embedded-java-${jdk.version}" {
-  passthru.unwrapped = jdk;
+  passthru.unwrapped = embedded-java;
 } ''
   mkdir -p $out/opt/tizen-studio
   ln -s ${jdk} $out/opt/tizen-studio/jdk
